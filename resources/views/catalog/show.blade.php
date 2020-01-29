ffe6f591eb035a0c6d62b870c10a78e7ec624821
Antonio Miguel Cobos Fernandez <cobosmoriles@gmail.com>
2020-01-29 10:26:49 +0100

    Show pelicula

--- resources/views/catalog/show.blade.php
+++ resources/views/catalog/show.blade.php
@@ -1,5 +1,35 @@
 @extends('layouts.master')
 
 @section('content')
-    Vista detalle película {id}
+
+    <div class="row">
+        <div class="col-sm-4">
+            <img src="{{$pelicula['poster']}}" style="height:500px"/>
+        </div>
+        <div class="col-sm-8">
+            <h2> {{$pelicula['title']}} </h2>
+            Año: {{$pelicula['year']}}<br>
+            Director: {{$pelicula['director']}}<br><br>
+            <b>Resumen: </b> {{$pelicula['synopsis']}}<br><br>
+            <b>Estado: </b>
+            @if($pelicula['rented'])
+                Pelicula actualmente alquilada <br><br>
+                <a style="color: white" class="btn btn-danger">Devolver película</a>
+            @else
+                Película disponible <br><br>
+                <a style="color: white" class="btn btn-danger">Alquilar película</a>
+            @endif
+
+            <a style="color: white" class="btn btn-warning" href="{{url('/catalog')}}">
+                <i class="fa fa-bed" aria-hidden="true" style="color: white"></i>
+                Editar película
+            </a>
+            <a class="btn btn-light" href="{{url('/catalog')}}">
+                <i class="fa fa-chevron-left" aria-hidden="true"></i>
+                Volver al listado
+            </a>
+
+        </div>
+    </div>
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', 'HomeController@getHome');
+
 
 Route::get('login', function () {
     return view('auth.login');
@@ -23,18 +22,14 @@ Route::get('logout', function () {
     echo "Logout usuario";
 });
 
-Route::get('catalog', function () {
-    return view('catalog.index');
-});
+Route::get('catalog', 'CatalogController@getIndex');
 
-Route::get('catalog/show/{id}', function ($id) {
-    return view('catalog.show', array('id' => $id));
-});
 
-Route::get('catalog/create', function () {
-    return view('catalog.create');
-});
+Route::get('catalog/show/{id}', 'CatalogController@getShow');
 
-Route::get('catalog/edit/{id}', function ($id) {
-    return view('catalog.edit', array('id' => $id));
-});
+
+Route::get('catalog/create', 'CatalogController@getCreate');
+
+
+Route::get('catalog/edit/{id}', 'CatalogController@getEdit');
+
