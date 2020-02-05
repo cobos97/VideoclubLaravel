df1ca3aa2cf2b300da68d7043caae70fe009d7c3
Antonio Miguel Cobos Fernandez <cobosmoriles@gmail.com>
2020-02-05 11:25:58 +0100

    Values de editar

--- resources/views/catalog/edit.blade.php
+++ resources/views/catalog/edit.blade.php
@@ -6,23 +6,23 @@
         {{method_field('PUT')}}
         <div class="form-group">
             <label for="title">Titulo</label>
-            <input type="text" class="form-control" id="title" name="title">
+            <input type="text" class="form-control" id="title" name="title" value="{{$pelicula->title}}">
         </div>
         <div class="form-group">
             <label for="year">Año</label>
-            <input type="text" class="form-control" id="year" name="year">
+            <input type="text" class="form-control" id="year" name="year" value="{{$pelicula->year}}">
         </div>
         <div class="form-group">
             <label for="director">Director</label>
-            <input type="text" class="form-control" id="director" name="director">
+            <input type="text" class="form-control" id="director" name="director" value="{{$pelicula->director}}">
         </div>
         <div class="form-group">
             <label for="poster">Poster</label>
-            <input type="text" class="form-control" id="poster" name="poster">
+            <input type="text" class="form-control" id="poster" name="poster" value="{{$pelicula->poster}}">
         </div>
         <div class="form-group">
             <label for="synopsis">Resumen</label>
-            <textarea class="form-control" id="synopsis" name="synopsis"></textarea>
+            <textarea class="form-control" id="synopsis" name="synopsis">{{$pelicula->synopsis}}</textarea>
         </div>
         <button type="submit" class="btn btn-primary" name="modificar">Modificar película</button>
     </form>
